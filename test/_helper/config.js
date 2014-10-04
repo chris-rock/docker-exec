ffd5d3afc17bd403f114635f55d9ca22cbbb4212
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-10-03 21:38:37 -0400

    check DOCKER_HOST is set or complain

--- test/_helper/config.js
+++ test/_helper/config.js
@@ -4,15 +4,19 @@ var os = require('os');
 var url = require('url');
 
 function getExecConfig() {
+    var config = null;
     if (os.platform() === 'darwin') {
-        var urlObj = url.parse(process.env.DOCKER_HOST);
-        return {
-            host: 'http://' + urlObj.hostname,
-            port: urlObj.port
-        };
-    } else {
-        return null; // default should work
+        if (process.env.DOCKER_HOST) {
+            var urlObj = url.parse(process.env.DOCKER_HOST);
+            config = {
+                host: 'http://' + urlObj.hostname,
+                port: urlObj.port
+            };
+        } else {
+            console.error('Please set environment variable DOCKER_HOST');
+        }
     }
+    return config;
 }
 
 module.exports = {
